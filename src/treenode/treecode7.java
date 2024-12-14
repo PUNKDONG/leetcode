f64265829b7e781eb0418379dcbe95def35a304e
zypc <1084753928@qq,com>
2024-12-14 22:00:44 +0800

    二叉树7

--- src/treenode/treecode7.java
+++ src/treenode/treecode7.java
@@ -18,12 +18,47 @@ public class treecode7
 }
 
     public static void main(String []args){
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        String[] values=s.substring(1,s.length()-1).split(",");
+       Scanner in=new Scanner(System.in);
+        String s = in.nextLine();
+        String[] split = s.substring(1, s.length() - 1).split(",");
+        int[] array = Arrays.stream(split).mapToInt(Integer::parseInt).toArray();
 
+        TreeNode helper = helper(array, 0, array.length - 1);
+
+        ArrayList<String> strings = new ArrayList<>();
+        chenxu(helper,strings);
+
+        System.out.println(strings);
 
     };
+     public static void chenxu(TreeNode root,List<String>  result){
+         if(root==null)return;
+         Queue<TreeNode> q=new LinkedList<>();
+         q.offer(root);
+         while(!q.isEmpty()){
+             int size = q.size();
+             while (size-->0){
+                 TreeNode poll = q.poll();
+                 if(poll!=null) {
+                     result.add(String.valueOf(poll.val));
+                     q.offer(poll.left);
+                     q.offer(poll.right);
+                 }
+                 else{
+                     result.add("null");
+                 }
+             }
+         }
+     }
+    public static TreeNode helper(int nums[],int left,int right){
+        if( left>right)return null;
+        int mid=(left+right)/2;
+        TreeNode treeNode = new TreeNode(nums[mid]);
+        treeNode.left=helper(nums,left,mid-1);
+        treeNode.right=helper(nums, mid+1,right );
+        return treeNode;
+
+    }
 
 
 }
---
+++ src/treenode/treecode8.java
@@ -0,0 +1,69 @@
+package treenode;
+
+
+
+import com.sun.source.tree.Tree;
+
+import java.util.*;
+
+public class treecode8{
+static class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val){this.val=val;}
+}
+public static void main(String[]args){
+    Scanner in=new Scanner(System.in);
+    String s = in.nextLine();
+    int k= in.nextInt();
+    String value[]=s.substring(1,s.length()-1).split(",");
+    TreeNode buildtree = Buildtree(value);
+
+    System.out.println(kthsmall(buildtree,k));
+}
+    public static int  kthsmall(TreeNode root,int k){
+        ArrayList<Integer> list = new ArrayList<>();
+        helper(root,list);
+        Collections.sort(list);
+        System.out.println(list);
+        return list.get(k-1);
+    }
+
+    public static void helper(TreeNode root, List<Integer> list){
+    if(root==null){return ;}
+
+     list.add(root.val );
+
+        helper(root.left,list);
+        helper(root.right,list);
+
+    }
+
+
+public static TreeNode Buildtree(String[]nums){
+    if(nums.length==0||nums[0].equals("null"))return null;
+    TreeNode root=new TreeNode(Integer.parseInt(nums[0]));
+    Queue<TreeNode> q=new LinkedList<>();
+    q.offer(root);
+    int index=1;
+    while(!q.isEmpty()&& index<nums.length){
+            TreeNode poll=q.poll();
+            if(!nums[index].equals("null")){
+                poll.left=new TreeNode(Integer.parseInt(nums[index]));
+                q.offer(poll.left);
+            }
+            index++;
+            if(index<nums.length&&!nums[index].equals("null")){
+                poll.right=new TreeNode(Integer.parseInt(nums[index]));
+                q.offer(poll.right);
+
+            }
+            index++;
+
+    }
+    return root;
+}
+
+
+}
